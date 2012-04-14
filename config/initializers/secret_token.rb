5895de193d2429df15deff41cc183484e7e738e2
Joaquin <joaquin_feria@yahoo.com>
2012-04-14 14:04:47 +0100

    Ficheros del FakeFacebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakeFacebook::Application.config.secret_token = '6bf1093926df98eb17ced8347c3eadfe0ac0922f5dcfe4e2b9610afe6cebadec8bec1ee9c72cc175d8a0388c8f32191be31f8a804d83c55d4777bcf776b829af'
